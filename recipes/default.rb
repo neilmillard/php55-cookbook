9c88d6220ed7056b3abe908cb38507590be084fa
neilmillard <neil.millard@gmail.com>
2014-07-25 12:50:32 +0100

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,32 +20,18 @@ case node[:platform]
     
     when "amazon"
     # node.set['php']['packages'] = ['php55w', 'php55w-devel', 'php55w-cli', 'php55w-snmp', 'php55w-soap', 'php55w-xml', 'php55w-xmlrpc', 'php55w-process', 'php55w-mysqlnd', 'php55w-pecl-memcache', 'php55w-opcache', 'php55w-pdo', 'php55w-imap', 'php55w-mbstring']
+    # we are going to use aws yum repo.
     node.set['mysql']['server']['packages'] = %w{mysql55-server}
     node.set['mysql']['client']['packages'] = %w{mysql55}
-    
-    # add the webtatic repository
-    yum_repository "webtatic" do
-        repo_name "webtatic"
-        description "webtatic Stable repo"
-        url "http://repo.webtatic.com/yum/el6/x86_64/"
-        key "RPM-GPG-KEY-webtatic-andy"
-        action :add
-    end
-    
-    yum_key "RPM-GPG-KEY-webtatic-andy" do
-        url "http://repo.webtatic.com/yum/RPM-GPG-KEY-webtatic-andy"
-        action :add
-    end
-    
     
     # remove any existing php/mysql
     execute "yum remove -y php* mysql*"
     
-    # get the metadata
+    # get the metadata - not really needed
     execute "yum -q makecache"
     
     # manually install php 5.5....
-    execute "yum install -y --skip-broken php55w php55w-devel php55w-cli php55w-snmp php55w-soap php55w-xml php55w-xmlrpc php55w-process php55w-mysqlnd php55w-pecl-memcache php55w-opcache php55w-pdo php55w-imap php55w-mbstring"
+    execute "yum install -y php55 php55-devel php55-cli php55-snmp php55-soap php55-xml php55-xmlrpc php55-process php55-mysqlnd php55-pecl-memcache php55-opcache php55-pdo php55-imap php55-mbstring"
 
   when "rhel", "fedora", "suse", "centos"
   # add the webtatic repository
